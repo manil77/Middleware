591ecd2c763e5e65f655dc4e872aea00b39c19cf
Manil Maharjan <manil@inficare.com.my>
2024-08-23 17:44:47 +0545

    Setup for Clean Architecture

---
+++ Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product> GetByIdAsync(int id);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Application.Interfaces
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        IProductRepository ProductRepository { get; }
+        Task<int> CompleteAsync();
+    }
+}
